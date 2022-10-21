1362b6f76eee4368cc14c55dcf7c3dab435b0969
yuanli <2865437316@qq.com>
2022-10-21 10:36:43 +0800

    default data

---
+++ src/constants/default.ts
@@ -0,0 +1,105 @@
+export const DEFAULT_DATA ={
+    basicInfo:{
+        userName:'',
+        avatar:'',
+        position:'',
+        gender:'',
+        email:'',
+        phone:'',
+        weChatNumber:'',
+        jobDate:'',
+        birthDate:'',
+        city:'',
+        salary:'',
+    },
+    advantages:{
+        value:""
+    },
+    education:[
+        {
+            schoolName:'',
+            qualification:'',
+            major:'',
+            timeSlot:'',
+            associationActivity:''
+        }
+    ],
+    // internship:[
+    //   {
+    //     corporateName:'',
+    //     In service time:'',
+    //     Position Type,
+    //   }
+    // ],
+}
+
+export const DEFAULT_PLACEHOLDER ={
+    basicInfo:{
+        userName:'请输入您的姓名',
+        avatar:'',
+        position:'请输入职位',
+        gender:'请选择性别',
+        email:'请输入您的邮箱',
+        phone:'请输入您的手机号',
+        weChatNumber:'请输入您的微信号',
+        jobDate:'请选择参加工作时间',
+        birthDate:'请选择出生日期',
+        city:'请选择期望城市',
+        salary:'请输入薪资',
+    },
+    advantages:"例如：两年UI设计经验，熟悉IOS和Android的界面设计规范，对产品本色有独特见解，有一定的手绘基础",
+    education:[
+        {
+            schoolName:'',
+            qualification:'',
+            major:'',
+            timeSlot:'',
+            associationActivity:''
+        }
+    ],
+    // internship:[
+    //   {
+    //     corporateName:'',
+    //     In service time:'',
+    //     Position Type,
+    //   }
+    // ],
+}
+
+export const DEFAULT_TITLE ={
+    basicInfo:{
+        userName:'姓名',
+        avatar:'头像',
+        position:'期望职位',
+        gender:'性别',
+        email:'邮箱',
+        phone:'电话',
+        weChatNumber:'微信号',
+        jobDate:'参加工作日期',
+        birthDate:'出生日期',
+        city:'期望工作城市',
+        salary:'薪资要求',
+    },
+    advantages:"个人优势",
+    education:[
+        {
+            schoolName:'',
+            qualification:'',
+            major:'',
+            timeSlot:'',
+            associationActivity:''
+        }
+    ],
+    // internship:[
+    //   {
+    //     corporateName:'',
+    //     In service time:'',
+    //     Position Type,
+    //   }
+    // ],
+}
+
+export const DEFAULT_MODULE_DISPLAY={
+    advantages:true,
+    education:true
+}
